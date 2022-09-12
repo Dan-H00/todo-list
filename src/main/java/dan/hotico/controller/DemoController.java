a813432caabb084a642b7b6527973ed78b573c82
Dan-H00 <hotico.dan00@yahoo.com>
2022-09-12 07:00:11 +0300

    fixed previous provlem

--- src/main/java/dan/hotico/controller/DemoController.java
+++ src/main/java/dan/hotico/controller/DemoController.java
@@ -12,4 +12,10 @@ public class DemoController {
     public String hello() {
         return "hello";
     }
+
+    // http://localhost:8080/todo-list/welcome
+    @GetMapping("welcome")
+    public String welcome(){
+        return "welcome";
+    }
 }
